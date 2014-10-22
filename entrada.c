a2b24ef42a680bbe4ef0923b23988485ad9dc4ed
Pablo Varela <pablovarela182@gmail.com>
2014-10-23 00:07:58 +0200

    Ahora podriamos usar N buffers en vez de solo 2

--- entrada.c
+++ entrada.c
@@ -11,12 +11,13 @@
 
 // Macros
 #define FILEMODE "r" // Modo de apertura del archivo
-#define N 10 // Tamano del buffer
+#define N 128 // Tamano del buffer
+#define MAXBUFFERS 2 // Numero de buffers que usamos
 
 // Variables globales
 char nombreArchivo[64];
 FILE* pFile; // puntero al archivo
-char* buffer[2]; // buffers de entrada ( 2 bloques por el metodo del centinela )
+char* buffer[MAXBUFFERS]; // buffers de entrada ( 2 bloques por el metodo del centinela )
 char* inicio; // Puntero al inicio del lexema
 char* delantero; // Puntero al caracter que estamos leyendo
 int leyendoBuffer=0; // Numero de buffer que leimos de ultimo
@@ -26,9 +27,7 @@ void cargarBuffer(int n)
 	int i=0;
 	char c;
 
-	if (n == 2) {
-		n = 0;
-	}
+	if (n == MAXBUFFERS) n = 0; // Volemos al primer buffer
 
 	leyendoBuffer = n; // Actualizamos el ultimo buffer leido
 
@@ -56,6 +55,7 @@ int leerArchivo()
 	buffer[0] = (char*) malloc((N+1) * sizeof(char));
 	buffer[1] = (char*) malloc((N+1) * sizeof(char));
 
+	// Cargo el primer buffer
 	cargarBuffer(0);
 
 	inicio = delantero = buffer[0]; // Inicio los punteros
