238b6809e3b32c76a77b94812fa3ced617364aff
Pablo Varela <pablovarela182@gmail.com>
2014-11-15 19:17:52 +0100

    Reconozco los strings

--- sintactico.c
+++ sintactico.c
@@ -20,21 +20,39 @@ int numlinea=1; /* Empezamos en la linea 1 no en la 0 */
 %}
 
 DIGITO	[0-9] // Numeros
-ID		[a-z][a-z0-9]*
+ID		[a-z|A-Z]*[a-z0-9|A-Z0-9]*
 
-%x comentario comentarioMono
+%x comentario comentarioMono string
 %%
 
 "#="	BEGIN(comentario);
 "#"		BEGIN(comentarioMono);
+"\"" {
+	BEGIN(string);
+	printf("<\""); // Imprimo las comillas iniciales
+}
+
 <comentario>[^=\n]*		/* ignora todo lo que no sea '=' */
-<comentario>"*"+[^=#\n]	/* ignora los '=' que no siguen con '#' */
 <comentario>\n			++numlinea; /* tambien contamos las lineas */
 <comentario>"="+"#"		BEGIN(INITIAL); /* salimos del comentario */
+
 <comentarioMono>[^\n]*	/* ignora todo hasta el salto de linea */
-<comentarioMono>\n {
+<comentarioMono>\n {	/* sumo el numero de linea y salgo del comentario */
 	++numlinea;
 	BEGIN(INITIAL);
+}
+
+<string>[^"\""]*	printf("%s", yytext); /* ignora todo hasta que no acabe el string pero lo imprimo */
+<string>\n			++numlinea;
+<string>"\"" {		/* imprimo las comillas finales y salgo del automata */
+	printf("\">");
+	BEGIN(INITIAL);
+}
+
+
+{ID} {
+	insertarTabla(yytext);
+	printf("<%s>", yytext);
 }
 
 %% 
