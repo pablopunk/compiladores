86aaf0edd28eae588aaab9e6cc524520a613c7ce
Pablo Varela <pablovarela182@gmail.com>
2014-11-15 18:53:07 +0100

    Comienzo con flex, ya reconozco los comentarios multilinea

--- makefile
+++ makefile
@@ -1,2 +1,8 @@
 all:
-	gcc -o compilador -g *.c
+	mv sintactico.c sintactico.flex && flex sintactico.flex && gcc -o compilador -g *.c -lfl && mv sintactico.flex sintactico.c
+
+osx:
+	mv sintactico.c sintactico.flex && flex sintactico.flex && gcc -o compilador -g *.c -ll && mv sintactico.flex sintactico.c
+
+linux:
+	mv sintactico.c sintactico.flex && flex sintactico.flex && gcc -o compilador -g *.c -lfl && mv sintactico.flex sintactico.c
--- sintactico.c
+++ sintactico.c
@@ -1,50 +1,53 @@
-//
-// Fichero de implementacion del analizador sintactico
-//
+
+%{
 
 // Librerias de C
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 // Librerias propias
-#include "lexico.h"
-#include "entrada.h"
 #include "tabla_simbolos.h"
 #include "definiciones.h"
 
+typedef struct {
+	char* lexema;
+	int numero;
+	int linea;
+} token;
+
+int numlinea=1; /* Empezamos en la linea 1 no en la 0 */
+
+%}
+
+DIGITO	[0-9] // Numeros
+ID		[a-z][a-z0-9]*
+
+%x comentario
+%%
+
+"#="	BEGIN(comentario);
+<comentario>[^=\n]*		/* ignora todo lo que no sea '=' */
+<comentario>"*"+[^=#\n]	/* ignora los '=' que no siguen con '#' */
+<comentario>\n			++numlinea; /* tambien contamos las lineas */
+<comentario>"="+"#"		BEGIN(INITIAL); /* salimos del comentario */
+
+%% 
+
 int main(int argc, char** argv)
 {
-	int out=0;
-	if (argc < 2) { // no hay argumentos de entrada
-		printf("Sin argumentos...\n");
-		return -1;
-	}
+	++argv, --argc;  /* se salta el nombre del programa */
+    if ( argc > 0 )
+        yyin = fopen( argv[0], "r" );
+    else
+        yyin = stdin;
+    
+    // Inicializamos la tabla de simbolos
+    inicializarTabla();
 
-	strcpy(nombreArchivo, argv[1]);
-	if ( (out = leerArchivo()) ) {
-		return out; // devuelvo el error que me devuelve la funcion
-	}
+    // Analisis lexico
+    yylex();
 
-	token* componente;
-
-	// Iniciamos la tabla de simbolos
-	inicializarTabla();
-	// Inicializamos el lexico
-	inicializarLexico();
-
-	while ( (componente = siguienteComponenteLexico()) && (componente->numero != EOF) ) {
-		if (componente->numero == ID && !busquedaTabla(componente->lexema)) { // Si es un ID lo metemos en la tabla
-			insertarTabla(componente->lexema);
-		}
-		printf("%i| <%i>\t%s\n", componente->linea, componente->numero, componente->lexema);
-	}
-
-	//imprimirTabla();
-
-	// Liberamos la tabla de simbolos
-	liberarTabla();
-	// Liberamos los componentes lexicos
-	liberarLexico();
+    printf("\nNumero de lineas: %d\n", numlinea);
 
 	return 0;
 }
