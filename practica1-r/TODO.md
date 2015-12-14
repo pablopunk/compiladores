e03872f30a3a958be55458099d89584123702c01
Pablo Varela <pablovarela182@gmail.com>
2015-12-14 08:02:26 +0100

    Opcion -static

--- practica1-r/TODO.md
+++ practica1-r/TODO.md
@@ -1,9 +1,4 @@
 
 ## To do
-- Añadir la opción del compilador `-static`
-  - añadir en el script (a parte de las demás)
-  - comprobar el tamaño del binario resultante
-  - cambiar los nombres de la tabla por *binario estatico* y *codigo objeto*
-
 
 - Ejercicio 3
--- practica1-r/compilar_y_ejecutar.sh
+++ practica1-r/compilar_y_ejecutar.sh
@@ -7,27 +7,32 @@ echo "Opciones;O0;O1;O2;O3;Os" > resultados/2h.csv
 # -O0
 gcc -O0 -S main.c -o O0/main.asm
 gcc -O0 -c main.c -o O0/main.o
+gcc -O0 -static main.c -o O0/main_static
 gcc -O0 main.c -o O0/bin
 # -O1
 gcc -O1 -S main.c -o O1/main.asm
 gcc -O1 -c main.c -o O1/main.o
+gcc -O1 -static main.c -o O1/main_static
 gcc -O1 main.c -o O1/bin
 # -O2
 gcc -O2 -S main.c -o O2/main.asm
 gcc -O2 -c main.c -o O2/main.o
+gcc -O2 -static main.c -o O2/main_static
 gcc -O2 main.c -o O2/bin
 # -O3
 gcc -O3 -S main.c -o O3/main.asm
 gcc -O3 -c main.c -o O3/main.o
+gcc -O3 -static main.c -o O3/main_static
 gcc -O3 main.c -o O3/bin
 # -Os
-gcc -Os -S main.c -o Os/main.asm
-gcc -Os -c main.c -o Os/main.o
-gcc -Os main.c -o Os/bin
+gcc -Os -S main.c -o 0S/main.asm
+gcc -Os -c main.c -o 0S/main.o
+gcc -Os -static main.c -o O4/main_static
+gcc -Os main.c -o 0S/bin
 
 ## resultados
 # Ejecucion
-echo "Tiempo;\c" >> resultados/2h.csv
+echo "Tiempo ejecución;\c" >> resultados/2h.csv
 ./O0/bin >> resultados/2h.csv
 echo ";\c" >> resultados/2h.csv
 ./O1/bin >> resultados/2h.csv
@@ -36,9 +41,9 @@ echo ";\c" >> resultados/2h.csv
 echo ";\c" >> resultados/2h.csv
 ./O3/bin >> resultados/2h.csv
 echo ";\c" >> resultados/2h.csv
-./Os/bin >> resultados/2h.csv
+./0S/bin >> resultados/2h.csv
 # Tamaño codigo objeto
-echo "\nTamaño;\c" >> resultados/2h.csv
+echo "\nTamaño c. objeto;\c" >> resultados/2h.csv
 ls -l O0/main.o | cut -d' ' -f8 | xargs echo -n >> resultados/2h.csv
 echo ";\c" >> resultados/2h.csv
 ls -l O1/main.o | cut -d' ' -f8 | xargs echo -n >> resultados/2h.csv
@@ -47,4 +52,15 @@ ls -l O2/main.o | cut -d' ' -f8 | xargs echo -n >> resultados/2h.csv
 echo ";\c" >> resultados/2h.csv
 ls -l O3/main.o | cut -d' ' -f8 | xargs echo -n >> resultados/2h.csv
 echo ";\c" >> resultados/2h.csv
-ls -l Os/main.o | cut -d' ' -f8 | xargs echo -n >> resultados/2h.csv
+ls -l 0S/main.o | cut -d' ' -f8 | xargs echo -n >> resultados/2h.csv
+# Tamaño codigo objeto
+echo "\nTamaño static bin;\c" >> resultados/2h.csv
+ls -l O0/main_static | cut -d' ' -f8 | xargs echo -n >> resultados/2h.csv
+echo ";\c" >> resultados/2h.csv
+ls -l O1/main_static | cut -d' ' -f8 | xargs echo -n >> resultados/2h.csv
+echo ";\c" >> resultados/2h.csv
+ls -l O2/main_static | cut -d' ' -f8 | xargs echo -n >> resultados/2h.csv
+echo ";\c" >> resultados/2h.csv
+ls -l O3/main_static | cut -d' ' -f8 | xargs echo -n >> resultados/2h.csv
+echo ";\c" >> resultados/2h.csv
+ls -l 0S/main_static | cut -d' ' -f8 | xargs echo -n >> resultados/2h.csv
