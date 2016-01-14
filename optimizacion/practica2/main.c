c63e39576e78dadaeff4b6243b05948575fa067e
Pablo Varela <pablovarela182@gmail.com>
2016-01-14 01:33:47 +0100

    que peligro la j

--- optimizacion/practica2/main.c
+++ optimizacion/practica2/main.c
@@ -4,37 +4,36 @@
 #include <sys/time.h>
 #include <ctype.h>
 
-#define ITER 10
+#define ITER 1000
 
 int main(int argc, char ** argv)
 {
   struct timeval inicio, final;
   double tiempo_sin_optimizar, tiempo_optimizado;
-  int N = 10;
+  int N = 100;
   register int i, j, k, l;
 
   argc--; argv++; // me salto el nombre del programa
   if ( argv[0] && isdigit(argv[0][0]) ) {
-    //N = atoi(argv[0]);
+    N = atoi(argv[0]);
   }
 
   float a[N][N], b[N][N], c[N][N];
   float aux;
-
   // enfriar cache
   for (l=0; l<ITER; l++)
     for (i=0; i<N; i++)
       for (j=0; j<N; j++)
-        for (k=0; j<N; k++) {
-          //aux = c[k][i];
-          //aux = a[k][j];
-          //aux = b[j][i];
+        for (k=0; k<N; k++) {
+          aux = c[k][i];
+          aux = a[k][j];
+          aux = b[j][i];
         }
   gettimeofday(&inicio, NULL); // inicio de codigo a medir
   for (l=0; l<ITER; l++)
     for (i=0; i<N; i++)
       for (j=0; j<N; j++)
-        for (k=0; j<N; k++)
+        for (k=0; k<N; k++)
           c[k][i] += a[k][j] * b[j][i];
   gettimeofday(&final, NULL); // fin de codigo a medir
 
@@ -44,7 +43,7 @@ int main(int argc, char ** argv)
   for (l=0; l<ITER; l++)
     for (i=0; i<N; i+=2)
       for (j=0; j<N; j+=2)
-        for (k=0; j<N; k++) {
+        for (k=0; k<N; k++) {
             c[k][i] += a[k][j] * b[j][i] + a[k][j+1] * b[j+1][i];
             c[k][i+1] += a[k][j] * b[j][i+1] + a[k][j+1] * b[j+1][i+1];
         }
